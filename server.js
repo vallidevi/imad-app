40443be9a64997dcc19c8503192e4b376f4a6a4d
vallidevi <30931707+vallidevi@users.noreply.github.com>
2017-08-22 11:05:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-1',function (req,res){
+    res.send('this is my first article');
+});
+app.get('/article-2',function (req,res){
+    res.send('this is my second article');
+});
+app.get('/article-3',function (req,res){
+    res.send('this is my third article');
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
