eabb5fdbe275f154d4bd81e01d1929047096d825
vallidevi <30931707+vallidevi@users.noreply.github.com>
2017-08-30 12:36:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
+var config ={
+    user:'vlldevi',
+    datbase:'vlldevi',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -14,6 +22,19 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/article-1',function (req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
+});
+var pool=new Pool(config);
+app.get('/test-db',function (req,res){
+    //make request
+    pool.query('SELECT * FROM test',function (err,result)
+    {
+    if (err){
+        res.status(500).send(err.toString());
+    }else{
+        res.send(JSON.stringify(result));
+    }
+    
+    });
 });
 app.get('/article-2',function (req,res){
  res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
